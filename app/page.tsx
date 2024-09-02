29a7050939bbeed23daca4c59f48c191e998e403
naPOME <natipomebiz1234@gmail.com>
2024-09-03 01:16:58 +0300

    day 1 check

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import '../styles/globals.css';
 import  NavBar  from "./components/common/navBar";
-import { Hero } from "./components/dashboard/hero";
+import Hero from "./components/dashboard/hero";
+
 export default function Home() {
   return (
    <>
