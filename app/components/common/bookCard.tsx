3160f079a3a8662445b824a8a76ddb491519e666
naPOME <natipomebiz1234@gmail.com>
2024-09-04 14:14:28 +0300

    book card

--- app/components/common/bookCard.tsx
+++ app/components/common/bookCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaBookmark } from 'react-icons/fa'; // Font Awesome bookmark icon
+import { FaBookmark, FaRedditAlien, FaShare, FaTelegram } from 'react-icons/fa';
 
 type BookCardProps = {
   id: string;
@@ -11,24 +11,26 @@ type BookCardProps = {
 
 const BookCard: React.FC<BookCardProps> = ({ id, title, author, image, onAddToReadingList }) => {
   return (
-    <div className="bg-white border border-gray-300 rounded-lg overflow-hidden shadow-sm hover:shadow-lg transition-shadow duration-300">
-      <div className="w-full h-60">
-        <img
-          src={image}
-          alt={title}
-          className="w-full h-full object-cover object-center transition-filter duration-300 grayscale hover:grayscale-0"
-        />
-      </div>
-      <div className="p-4">
-        <h3 className="text-lg font-bold text-black">{title}</h3>
-        <p className="text-gray-500">{author}</p>
+    <div className="bg-transparent min-w-48  rounded-lg p-4 ">
+      <img src={image} alt={title} className="w-48   object-cover rounded-md" />
+      <div className="mt-4 ">
+        <h3 className="text-lg font-bold text-black ">{title}</h3>
+        <p className="text-gray-600 ">{author}</p>
         <button
           onClick={() => onAddToReadingList(id)}
-          className="mt-4 px-4 py-2 w-full bg-white text-black border border-black rounded hover:bg-black hover:text-white transition-colors duration-300 flex items-center justify-center"
+          className="mt-4 bg-black text-white py-2 px-4 rounded hover:bg-gray-800 ml-5 "
         >
-          <FaBookmark className="mr-2" />
-          Add to Reading List
+          <FaBookmark />
+          
         </button>
+        <button
+          onClick={() => onAddToReadingList(id)}
+          className="mt-4 bg-black text-white py-2 px-4 ml-12 rounded hover:bg-gray-800 "
+        >
+          <FaShare/>
+          
+        </button>
+        
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import  NavBar  from "./components/common/navBar";
 import {Hero} from "./components/dashboard/hero";
 
 import GoogleBooks from "@/pages/bookSection";
+import BookSection from "@/pages/bookSection";
 
 export default function Home() {
   return (
    <>
    <NavBar/>
    <Hero/>
-   <GoogleBooks/>
+   <BookSection/>
    </>
   );
 }
